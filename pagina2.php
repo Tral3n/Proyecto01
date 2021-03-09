134c9e3eb351f6cc0a6c5ae8ab753f866592aab9
Tral3n <tralendk@gmail.com>
2021-03-09 16:32:59 -0500

    Soy bobo y mande el php vacio

---
+++ pagina2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+<title>Proyecto heroku</title>
+<meta chatset="utf-8"/>
+<link rel = "stylesheet" content="text/css" href="css/normalize.css"/>
+<link rel = "stylesheet" content="text/css" href="css/estilos.css"/>
+</head>
+<body>
+
+<?php
+$a= 12;
+$b =1000;
+$a.=$b;
+echo "<br/> Concatenado $a ."
+
+
+
+?>
+</body>
+</html>
