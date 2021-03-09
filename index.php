993d00a2f17d1abf26135efbbdd0f6130cdf0f04
Tral3n <tralendk@gmail.com>
2021-03-09 15:41:09 -0500

    Primer commit, se creo proyecto01 con un directorio css que tiene 2 achivos css y u archivo index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<title>PROYECTO HEROKU</title>
+<meta charset="utf.8"/>
+<link rel="stylesheet" content="text/css" href="css/normalize.css" />
+<link rel="stylesheet" content="text/css" href="css/estilos.css" />
+</head>
+
+<body>
+
+<div id="envoltura">
+<header>
+    <h1>Programaci&oacute;n Web</h1>
+</header>
+<div id="main"> 
+    <p>
+    
+    En este curso aprender&aacute;s a programar p&aacute;ginas web usando git.
+    <?php 
+    
+    
+  $curso = "Construccion de elementos de software 2 ";
+  echo"<br /> <br /> ESTAS EN EL CURSO: $curso ."
+    
+    
+    
+    ?>
+    </p>
+</div>
+
+
+</body>
+
+
+
+
+
+
+
+</html>
+
